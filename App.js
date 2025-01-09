4f1605bde7c8237b513862a4b214a9890a599b1b
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2025-01-09 23:56:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils2" aboutText="TextUtils" />
+    <div className="container">
+
+    <TextForm heading="Enter the  text to analyze below"/>
+    </div>
+    
+    
+    
+    
+    </>
+    
+    
+    
+  );
+}
+
+export default App;
